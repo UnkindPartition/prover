7d368d482baa381d7b4c870cffd40d6e49fae1ad
Roman Cheplyaka <roma@ro-che.info>
2015-05-26 18:54:54 +0300

    Wiggle: use FSum

--- src/Wiggle.hs
+++ src/Wiggle.hs
@@ -4,13 +4,41 @@
 module Wiggle (W(..), wiggled, wapply) where
 
 import Control.Applicative
+import Control.Monad.Logic
+import Data.Functor.Compose
+
+----------------------------------------------------------------------
+--                           FSum class
+----------------------------------------------------------------------
+
+-- | Like Alternative, but with no Applicative superclass
+-- constraint.
+--
+-- This is important, as Compose Logic Reduction is a functor and FSum, but
+-- not Applicative (or Alternative).
+class Functor f => FSum f where
+  fempty :: f a
+
+  default fempty :: Alternative f => f a
+  fempty = empty
+  
+  fplus :: f a -> f a -> f a
+
+  default fplus :: Alternative f => f a -> f a -> f a
+  fplus = (<|>)
+
+instance FSum Logic
+
+instance (FSum f, Functor g) => FSum (Compose f g) where
+  fempty = Compose fempty
+  fplus (Compose a) (Compose b) = Compose (fplus a b)
 
 data W t a = W (t a) a
 instance Functor t => Functor (W t) where
     fmap f (W xs x) = W (fmap f xs) (f x)
-instance Alternative t => Applicative (W t) where
-    pure a = W empty a
-    W fs f <*> W xs x = W (fmap ($ x) fs <|> fmap f xs) (f x)
+instance FSum t => Applicative (W t) where
+    pure a = W fempty a
+    W fs f <*> W xs x = W (fmap ($ x) fs `fplus` fmap f xs) (f x)
 
 wiggled :: W t a -> t a
 wiggled (W xs _) = xs
